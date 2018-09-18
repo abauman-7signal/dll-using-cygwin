06c443a80de40ec71fd5edd6fbb348d635140b67
Aaron Bauman <aaron.bauman@7signal.com>
2018-09-18 11:21:56 -0400

    compile DLL with c++ and export DLL API

--- build.sh
+++ build.sh
@@ -1,17 +1,31 @@
 #!/bin/sh
 
+MODULE=HelloWorld
+
+function processStatusOfLastCommand() {
+  if [ $? -ne 0 ] ; then
+    echo "F A I L E D"
+    exit 1
+  fi
+}
+
 function compile() {
   echo ---------------------
   echo   C O M P I L I N G
   echo ---------------------
-  gcc -v -c HelloWorld.cpp
+  g++ -v -c ${MODULE}.cpp
+  processStatusOfLastCommand
 }
 
 function link() {
   echo -----------------
   echo   L I N K I N G
   echo -----------------
-  gcc -v -shared -o HelloWorld.dll HelloWorld.o
+  g++ -v -shared -o ${MODULE}.dll ${MODULE}.o \
+      -Wl,--out-implib=lib${MODULE}.dll.a \
+      -Wl,--export-all-symbols \
+      -Wl,--enable-auto-import
+  processStatusOfLastCommand
 }
 
 
