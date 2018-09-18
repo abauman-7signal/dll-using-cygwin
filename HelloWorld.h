de8d2ff5272feaa1bcad965cbba1faaa82f78403
Aaron Bauman <aaron.bauman@7signal.com>
2018-09-18 08:56:29 -0400

    adds include file for DLL implementation

---
+++ HelloWorld.h
@@ -0,0 +1,16 @@
+#ifndef _Included_HelloWorld
+#define _Included_HelloWorld
+// #ifdef __cplusplus
+extern "C" {
+// #endif
+/*
+ * Class:     HelloWorld
+ * Method:    print
+ * Signature: ()V
+ */
+int printHello(void);
+
+// #ifdef __cplusplus
+}
+// #endif
+#endif
